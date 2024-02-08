4bdf8bf1b39e89bd8792ee282e1bac92235171b1
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-02-08 20:30:35 +0900

    Implement a translation method for server side

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,14 @@
+import { getTranslation } from '@/app/i18n/server';
+
 export default async function Home({ params }: { params: { lang: string } }) {
   const lang = params.lang;
+  const { t } = await getTranslation(lang);
 
   return (
     <main>
-      <div className="m-5">{lang}</div>
+      <div className="m-5">
+        <h1>{t('app_name')}</h1>
+      </div>
     </main>
   );
 }
--- src/app/i18n/settings.ts
+++ src/app/i18n/settings.ts
@@ -1,2 +1,14 @@
 export const defaultLanguage = 'ja';
 export const availableLanguages = [defaultLanguage, 'en'];
+export const namespaces = ['translation'];
+
+export function getOptions(lng = defaultLanguage) {
+  return {
+    lng,
+    defaultNS: defaultLanguage,
+    fallbackLng: defaultLanguage,
+    fallbackNS: namespaces[0],
+    ns: namespaces,
+    supportedLngs: availableLanguages,
+  };
+}
